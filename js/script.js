13d4ad9c2535807e5b5a441a3b1c757811231ba4
henri-kapidani <h.kapidani@gmail.com>
2022-10-19 15:13:08 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// document.writeln('ciao a tutti')
+
+// alert('riciao a tutti')
+
+// document.getElementById('result').innerHTML = 'ciao a tutti';
+
+document.getElementById('result').innerHTML = document.getElementById('saluto-originale').innerHTML;
+
+document.getElementById('result').innerHTML = '<a href="#!">questo è un link</a>';
+
+// console.log('ciao da console.log');
+
+
+const saluto = 'ciao';
+const saluto2 = 'a tutti';
+
+// console.log(saluto);
+// console.log(saluto2);
+
+console.log(saluto + ' ' + saluto2);
+
+console.log(`${saluto} ${saluto2}`)
+
+console.log()
